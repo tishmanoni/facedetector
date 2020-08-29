f53df694e9015a75fcad7f2cca7e2c4b57b3309c
tishmanoni <66375712+tishmanoni@users.noreply.github.com>
2020-08-29 19:43:39 +0100

    added new files

--- mystock/stock/admin.py
+++ mystock/stock/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Myuser
+# Register your models here.
+admin.site.register(Myuser)
--- mystock/stock/models.py
+++ mystock/stock/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Myuser(models.Model):
+    firstname = models.TextField()
+    lastname = models.TextField()
